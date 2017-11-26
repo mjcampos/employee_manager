ec9fdc9e62aff0876951815f0c8b5b31e25ae81b
Michael Campos <camposmj89@gmail.com>
2017-11-26 10:49:44 -0800

    Added LoginForm component

--- src/App.js
+++ src/App.js
@@ -4,27 +4,24 @@ import {Provider} from 'react-redux';
 import {createStore} from 'redux';
 import reducers from './reducers';
 import firebase from 'firebase';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
 	componentWillMount() {
-		  var config = {
-		    apiKey: "AIzaSyB9HbUwAjJPLcAfJIIG5u0X-l2SfR6lEoY",
-		    authDomain: "manager-88660.firebaseapp.com",
-		    databaseURL: "https://manager-88660.firebaseio.com",
-		    projectId: "manager-88660",
-		    storageBucket: "manager-88660.appspot.com",
-		    messagingSenderId: "670533674916"
-		  };
-
-		  firebase.initializeApp(config);
+		firebase.initializeApp({
+			apiKey: "AIzaSyB9HbUwAjJPLcAfJIIG5u0X-l2SfR6lEoY",
+			authDomain: "manager-88660.firebaseapp.com",
+			databaseURL: "https://manager-88660.firebaseio.com",
+			projectId: "manager-88660",
+			storageBucket: "manager-88660.appspot.com",
+			messagingSenderId: "670533674916"
+		});
 	}
 
 	render() {
 		return (
 			<Provider store={createStore(reducers)}>
-				<View>
-					<Text>Hello there</Text>
-				</View>
+				<LoginForm/>
 			</Provider>
 		);
 	}
