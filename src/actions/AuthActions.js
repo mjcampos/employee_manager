27442b3fa804818b979cd2288a0770918bb1b823
Michael Campos <camposmj89@gmail.com>
2017-11-30 17:30:11 -0800

    Seperated form and authentication actions into their own action files that are connected together in an index.js file

---
+++ src/actions/AuthActions.js
@@ -0,0 +1,51 @@
+import firebase from 'firebase';
+import {Actions} from 'react-native-router-flux';
+import {EMAIL_CHANGED, PASSWORD_CHANGED, LOGIN_USER_SUCCESS, LOGIN_USER_FAIL, LOGIN_USER} from './types';
+
+export const emailChanged = email => {
+	return {
+		type: EMAIL_CHANGED,
+		email
+	}
+}
+
+export const passwordChanged = password => {
+	return {
+		type: PASSWORD_CHANGED,
+		password
+	}
+}
+
+export const loginUserSuccess = user => {
+	Actions.main();
+
+	return {
+		type: LOGIN_USER_SUCCESS,
+		user
+	}
+}
+
+export const loginUserFail = (error) => {
+	return {
+		type: LOGIN_USER_FAIL,
+		error
+	}
+}
+
+export const logingInUser = () => {
+	return {
+		type: LOGIN_USER
+	}
+}
+
+export const loginUser = ({email, password}) => dispatch => {
+	dispatch(logingInUser());
+
+	firebase.auth().signInWithEmailAndPassword(email, password)
+		.then(user => dispatch(loginUserSuccess(user)))
+		.catch(() => {
+			firebase.auth().createUserWithEmailAndPassword(email, password)
+				.then(user => dispatch(loginUserSuccess(user)))
+				.catch(error => dispatch(loginUserFail(error.message)));
+		});
+}
