44293f10292a83edec2ba5c65706c093ca77fa16
Michael Campos <camposmj89@gmail.com>
2017-12-01 06:05:23 -0800

    Set up EmployeeForm reducer and connected to to EmployeeCreate component through actions

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,14 +1,21 @@
 import React, {Component} from 'react';
+import {Picker, Text} from 'react-native';
 import {Card, CardSection, Input, Button} from './common';
+import {connect} from 'react-redux';
+import {employeeUpdate} from '../actions'
 
 class EmployeeCreate extends Component {
 	render() {
+		var {name, phone} = this.props;
+
 		return (
 			<Card>
 				<CardSection>
 					<Input
 						label="Name"
 						placeholder="Jane"
+						value={name}
+						onChangeText={value => this.props.employeeUpdate({prop: 'name', value})}
 					/>
 				</CardSection>
 
@@ -16,10 +23,24 @@ class EmployeeCreate extends Component {
 					<Input
 						label="Phone"
 						placeholder="555-555-5555"
+						value={phone}
+						onChangeText={value => this.props.employeeUpdate({prop: 'phone', value})}
 					/>
 				</CardSection>
 
-				<CardSection></CardSection>
+				<CardSection style={{flexDirection: 'column'}}>
+					<Text style={styles.pickerLabelStyle}>Shift</Text>
+
+					<Picker selectedValue={this.props.shift} onValueChange={value => this.props.employeeUpdate({prop: 'shift', value})} style={{flex: 1}}>
+						<Picker.Item label="Monday" value="Monday"/>
+						<Picker.Item label="Tuesday" value="Tuesday"/>
+						<Picker.Item label="Wendnesday" value="Wendnesday"/>
+						<Picker.Item label="Thursday" value="Thursday"/>
+						<Picker.Item label="Friday" value="Friday"/>
+						<Picker.Item label="Saturday" value="Saturday"/>
+						<Picker.Item label="Sunday" value="Sunday"/>
+					</Picker>
+				</CardSection>
 
 				<CardSection>
 					<Button>Create</Button>
@@ -29,4 +50,21 @@ class EmployeeCreate extends Component {
 	}
 }
 
-export default EmployeeCreate;
+const styles = {
+	pickerLabelStyle: {
+		fontSize: 18,
+		paddingLeft: 20
+	}
+}
+
+function mapStateToProps(state) {
+	var {name, phone, shift} = state.employeeForm;
+
+	return {
+		name,
+		phone,
+		shift
+	}
+}
+
+export default connect(mapStateToProps, {employeeUpdate})(EmployeeCreate);
--- src/components/common/CardSection.js
+++ src/components/common/CardSection.js
@@ -3,7 +3,7 @@ import {View} from 'react-native';
 
 var CardSection = (props) => {
 	return (
-		<View style={styles.containerStyle}>
+		<View style={[styles.containerStyle, props.style]}>
 			{props.children}
 		</View>
 	);
