2a24e47be1f513deea6f41c98ac4343e5ab2d342
Michael Campos <camposmj89@gmail.com>
2017-12-03 08:40:29 -0800

    Installed npm package react-native-communications to send messages in the user's messaging app

--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -1,5 +1,6 @@
 import _ from 'lodash';
 import React, {Component} from 'react';
+import Communcations from 'react-native-communications';
 import {connect} from 'react-redux';
 import EmployeeForm from './EmployeeForm';
 import {employeeUpdate, employeeSave} from '../actions';
@@ -18,6 +19,12 @@ class EmployeeEdit extends Component {
 		this.props.employeeSave({name, phone, shift, uid: this.props.employee.uid});
 	}
 
+	onTextPress() {
+		const {phone, shift} = this.props;
+
+		Communcations.text(phone, `Your upcoming shift is on ${shift}`);
+	}
+
 	render() {
 		return (
 			<Card>
@@ -25,6 +32,10 @@ class EmployeeEdit extends Component {
 
 				<CardSection>
 					<Button onPress={this.onButtonPress.bind(this)}>Save Changes</Button>
+				</CardSection>
+
+				<CardSection>
+					<Button onPress={this.onTextPress.bind(this)}>Text Schedule</Button>
 				</CardSection>
 			</Card>
 		);
