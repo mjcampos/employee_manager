7bbcbf9a942c2c7924386856dbf0fec16dda4c45
Michael Campos <camposmj89@gmail.com>
2017-12-03 11:47:24 -0800

    Build out Confirm component and imported it to EmployeeEdit component

--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -4,9 +4,13 @@ import Communcations from 'react-native-communications';
 import {connect} from 'react-redux';
 import EmployeeForm from './EmployeeForm';
 import {employeeUpdate, employeeSave} from '../actions';
-import {Card, CardSection, Button} from './common';
+import {Card, CardSection, Button, Confirm} from './common';
 
 class EmployeeEdit extends Component {
+	state = {
+		showModal: false
+	}
+
 	componentWillMount() {
 		_.each(this.props.employee, (value, prop) => {
 			this.props.employeeUpdate({prop, value});
@@ -39,8 +43,12 @@ class EmployeeEdit extends Component {
 				</CardSection>
 
 				<CardSection>
-					<Button>Fire</Button>
+					<Button onPress={() => this.setState({showModal: !this.state.showModal})}>Fire Employee</Button>
 				</CardSection>
+
+				<Confirm
+					visible={this.state.showModal}
+				>Are you sure you want to delete this?</Confirm>
 			</Card>
 		);
 	}
